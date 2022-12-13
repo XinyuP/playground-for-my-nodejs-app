8b351682ff9536499e73804fb21e55146bd5bb23
XinyuP <xinyup@umich.edu>
2022-12-13 06:03:54 -0500

    es6 object property shorthand and destructuring

--- 4-callbacks.js
+++ 4-callbacks.js
@@ -13,56 +13,42 @@
 //     }, 2000);
 // }
 
-
 const add = (a, b, callback) => {
-    setTimeout(() => {callback(a+b)}, 2000)
-}
-
+	setTimeout(() => {
+		callback(a + b);
+	}, 2000);
+};
 
 add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
-})
+	console.log(sum); // Should print: 5
+});
 
+setTimeout(() => {
+	console.log('2s');
+}, 2000);
 
+// setTimeout is a node-provided API and it is asynchronous
+// array method like forEach and filter also use callback but synchronous
 
+const names = ['Blaire', 'Ted', 'Cindy', 'Alexander'];
+const shortName = names.filter((name) => name.length <= 5);
+console.log(shortName);
 
+const geocode = (address, callback) => {
+	setTimeout(() => {
+		const data = {
+			latitude: 0,
+			longitude: 0,
+		};
+		callback(data);
+	}, 2000);
+};
 
+geocode('Beijing', (dataaaa) => {
+	console.log(dataaaa);
+});
 
-// setTimeout(() => {
-//     console.log('2s')
-// }, 2000);
-
-// // setTimeout is a node-provided API and it is asynchronous
-// // array method like forEach and filter also use callback but synchronous
-
-// const names = ['Blaire', 'Ted', 'Cindy', 'Alexander']
-// const shortName = names.filter((name) => name.length <= 5)
-// console.log(shortName)
-
-
-// const geocode = (address, callback) => {
-//     setTimeout(() => {
-//         const data = {
-//             latitude: 0,
-//             longitude: 0
-//         }
-//         callback(data) 
-//     }, 2000);
-// }
-
-// geocode('Beijing', (dataaaa) => {
-//     console.log(dataaaa)
-// })
-
-
-
-
-
-
-
-
-// no callback functions get executed until the call stack is empty ---> mean() has to finish first 
-
+// no callback functions get executed until the call stack is empty ---> mean() has to finish first
 
 // const geocode = (address, callback) => {
 //     setTimeout(() => {
@@ -73,7 +59,6 @@ add(1, 4, (sum) => {
 //         return data
 //     }, 2000);
 // }///// not work :(((((
-
 
 // a callback function is a function we provide as an argument to another function
 // with intention of having it called later on
---
+++ 5-es6-objects.js
@@ -0,0 +1,39 @@
+// Object property shorthand
+const name = 'Blaire';
+const userAge = 21;
+
+// // const user = {
+// //     name: name,
+// //     age: userAge,
+// //     location: 'Ann Arbor'
+// // }
+
+const user = {
+	name,
+	age: userAge,
+	location: 'Ann Arbor',
+};
+
+console.log(user);
+
+//Object destructuring
+
+const product = {
+	lebal: 'notebook',
+	price: 3,
+	stock: 200,
+	salePrice: undefined,
+	//rating: 23
+};
+
+// const { lebal, stock} = product
+// console.log(lebal, stock)
+
+// const { lebal: pppplebal, stock: uuustock, rating = 5} = product // rename | set up default
+// console.log(pppplebal, uuustock, rating)
+
+const transaction = (type, { lebal, stock }) => {
+	console.log(type, lebal, stock);
+};
+
+transaction('order', product);
