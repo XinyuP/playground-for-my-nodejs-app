32b9ca4b781f0dc0537c60198529fb8dce9799a1
XinyuP <xinyup@umich.edu>
2022-12-12 23:13:58 -0500

    use prettier

--- 1-json.js
+++ 1-json.js
@@ -1,19 +1,18 @@
-const fs = require('fs')
+const fs = require('fs');
 
-
-const dataBuffer = fs.readFileSync('1-json.json')  // binary
-const dataJSON = dataBuffer.toString() // binary to string
-const user = JSON.parse(dataJSON) // string to object 
-user.name = "Blaire" // change object
-user.age = 21 
-const userJSON = JSON.stringify(user) // object to string
-fs.writeFileSync('1-json.json', userJSON)
+const dataBuffer = fs.readFileSync('1-json.json'); // binary
+const dataJSON = dataBuffer.toString(); // binary to string
+const user = JSON.parse(dataJSON); // string to object
+user.name = 'Blaireeee'; // change object
+user.age = 21;
+const userJSON = JSON.stringify(user); // object to string
+fs.writeFileSync('1-json.json', userJSON);
 // const book = {
 //     title: 'Beauty',
 //     author: 'Blaire'
 // }
 
-// const bookJSON = JSON.stringify(book) // convert onject to string 
+// const bookJSON = JSON.stringify(book) // convert onject to string
 
 // fs.writeFileSync('1-json.json', bookJSON)
 
@@ -25,12 +24,8 @@ fs.writeFileSync('1-json.json', userJSON)
 // const data = JSON.parse(dataJSON)
 
 // console.log(data) // json object
-// console.log(data.title)  // access property 
-
+// console.log(data.title)  // access property
 
 // console.log(bookJSON)
 // const parsedData = JSON.parse(bookJSON) // convert json data to object
 // console.log(parsedData)
-
-
-
